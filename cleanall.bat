46ad2495054a348fb8200746494f5ad1a215b950
Roberto Camacho Barranco <crobertob@hotmail.com>
2013-07-10 13:14:12 +0200

    Revert "Small fix to geometry"
    This reverts commit a0f5670c9a5492f6ce6158e82b21348e82285083.

--- cleanall.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\Roberto\Documents\GitHub\finalProject
-
-del *.o
-
-del *.vtk
--- run3scenarios.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Roberto\Documents\GitHub\finalProject
-sim.exe FlowOverStep
-sim.exe KarmanVortexStreet
-sim.exe PlaneShearFlow
